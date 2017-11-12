ccc7a9c386ef455fb542eafd57d660ad0473990c
Rajat Garga <gargarajat@outlook.com>
2017-11-12 09:41:40 +0530

    reyuf

--- app/src/main/java/com/utini/utini/MainActivity.java
+++ app/src/main/java/com/utini/utini/MainActivity.java
@@ -2,102 +2,94 @@ package com.utini.utini;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.github.mikephil.charting.charts.BarChart;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+    TextView item1, item2, item3;
+    TextView cost1, cost2, cost3;
+
+    private BarChart mChart;
+    List < MonthlyData > DataList;
+    TextView expecditure, saving, forecastExpenditure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        setSupportActionBar(toolbar);
+        item1 = findViewById(R.id.name1);
+        item2 = findViewById(R.id.name2);
+        item3 = findViewById(R.id.name3);
+        cost1 = findViewById(R.id.amnt1);
+        cost2 = findViewById(R.id.amnt2);
+        cost3 = findViewById(R.id.amnt3);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        expecditure = findViewById(R.id.exp);
+        saving = findViewById(R.id.saving);
+        forecastExpenditure = findViewById(R.id.exp_forcast);
+
+        ImageView newtrans = findViewById(R.id.newtrans);
+        newtrans.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, form.class);
+                startActivity(intent);
+                finish();
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+        String dataString = getApplicationContext().getSharedPreferences("MyPref", 0).getString("Data", null);
+        if(dataString == null){
+            Log.i("PREFRENCES EMPTY", "NO DATA");
+        }else{
+            Log.i("Le BC", dataString);
+            Type type = new TypeToken<List< MonthlyData >>() {}.getType();
+            DataList = new Gson().fromJson(dataString, type);
+            GraphView graph = (GraphView) findViewById(R.id.graph);
+            DataPoint[] dataArray = new DataPoint[DataList.size()];
+            int j=0;
+            for(int i=0; i<DataList.size(); i++){
+                dataArray[j] = new DataPoint(DataList.get(i).getMonth(), DataList.get(i).getSavings());
+                j++;
+            }
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataArray);
+            graph.getGridLabelRenderer().setGridStyle( GridLabelRenderer.GridStyle.NONE );
+            graph.getViewport().setDrawBorder(true);
+            graph.addSeries(series);
+            String expe = Double.toString(DataList.get(DataList.size()-1).getExpenditure());
+            Log.i("Expenditure", expe);
+            String savi = Double.toString(DataList.get(DataList.size()-1).getSavings());
+            Log.i("Saving", savi);
+            String est = Double.toString(DataList.get(DataList.size()-1).getMonthEstimate());
+            Log.i("Estimate", est);
+            String  max = Double.toString(DataList.get(DataList.size()-1).getMaxExpenditure());
+            Log.i("MAX Expenditure BITCHES", max);
+            expecditure.setText(expe);
+            saving.setText(savi);
+            forecastExpenditure.setText(est);
+            DataList.get(DataList.size()-1).sort();
+            cost1.setText(Double.toString(DataList.get(DataList.size()-1).sorted[0].amount));
+            cost2.setText(Double.toString(DataList.get(DataList.size()-1).sorted[1].amount));
+            cost3.setText(Double.toString(DataList.get(DataList.size()-1).sorted[2].amount));
+            item1.setText(DataList.get(DataList.size()-1).sorted[0].source);
+            item2.setText(DataList.get(DataList.size()-1).sorted[1].source);
+            item3.setText(DataList.get(DataList.size()-1).sorted[2].source);
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.saving) {
-            // Handle the camera action
-            Intent intent=new Intent(MainActivity.this, MySavings.class);
-            startActivity(intent);
-        } else if (id == R.id.product) {
-
-        } else if (id == R.id.monthly) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_about) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
--- app/src/main/java/com/utini/utini/MySavings.java
+++ app/src/main/java/com/utini/utini/MySavings.java
@@ -2,51 +2,15 @@ package com.utini.utini;
 
 import android.os.Bundle;
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-
-import java.util.ArrayList;
-
 /**
  * Created by garga on 11/12/2017.
  */
 
 public class MySavings extends MainActivity {
 
-    private BarChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_savings);
-
-        mChart = findViewById(R.id.graphBar);
-
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1f, 10));
-        barEntries.add(new BarEntry(2f, 20));
-        barEntries.add(new BarEntry(3f, 30));
-        barEntries.add(new BarEntry(4f, 40));
-        barEntries.add(new BarEntry(5f, 50));
-        BarDataSet entriset = new BarDataSet(barEntries, "Dates");
-
-        ArrayList<String> months = new ArrayList<>();
-        months.add("April");
-        months.add("May");
-        months.add("June");
-        months.add("July");
-        months.add("August");
-
-        BarData sexyData = new BarData(entriset);
-        mChart.setPinchZoom(false);
-        mChart.setDoubleTapToZoomEnabled(false);
-        mChart.setScaleEnabled(false);
-        mChart.setTouchEnabled(false);
-        mChart.setDrawBorders(false);
-        mChart.setEnabled(true);
-        mChart.setGridBackgroundColor(020040);
-        mChart.setData(sexyData);
-    }
-}
+    }}
